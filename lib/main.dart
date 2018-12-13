7a09d4c51a6af4f420e42c64baad9624c617a5e5
yangjianan <yangjianan@seuic.com>
2018-12-13 12:33:38 +0800

    ListView1

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData.light(),
+    // TODO: implement build
+    return MaterialApp(
+      title: 'JSPang Flutter Demo',
       home: Scaffold(
-        body: Center(
-          child: Container(
-            child: Image.network(
-              "https://i2.hdslb.com/bfs/face/0cca7e53848ebd079e1b55fde929a473a882ee7f.jpg@68w_68h.webp",
-              // fit: BoxFit.contain, //图片填充模式
-              // color: Color(0xffff0000), //颜色混合
-              // colorBlendMode: BlendMode.darken, //混合模式
-              repeat: ImageRepeat.repeatX,
-              ),
-            width: 300.0,
-            height: 200.0,
-            color: Colors.lightBlue,
-          ),
+        appBar: AppBar(
+          title: Text('ListView Widget'),
+        ),
+        body: ListView(
+          children: <Widget>[
+            new Image.network(
+                'http://jspang.com/static/upload/20181109/1bHNoNGpZjyriCNcvqdKo3s6.jpg'),
+            new Image.network(
+                'http://jspang.com/static/upload/20181111/G-wj-ZQuocWlYOHM6MT2Hbh5.jpg'),
+            new Image.network(
+                'http://jspang.com/static/upload/20181126/FB4kjZaGt48a4HzYakHWQpU7.png'),
+            new ListTile(
+              leading: Icon(Icons.ac_unit),
+              title: Text('listview item1'),
+            ),
+            new ListTile(
+              leading: Icon(Icons.access_alarm),
+              title: Text('listview item2'),
+            ),
+            new ListTile(
+              leading: Icon(Icons.adb),
+              title: Text('listview item3'),
+            )
+          ],
         ),
       ),
     );
