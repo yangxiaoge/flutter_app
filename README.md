896f00e54c0abbdb7c8656ec41bb30dd9f1f8570
yangjianan <yangjianan@seuic.com>
2018-12-13 09:18:23 +0800

    跟着技术胖Flutter视频学习

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ A new Flutter application.
 
 For help getting started with Flutter, view our online
 [documentation](https://flutter.io/).
+
+## 技术胖哔哩哔哩视频
+[视频地址](https://www.bilibili.com/video/av35800108?p=8)
+
+![](/screenshot/JSPang_Videos.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,137 +1,29 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-// main 函数使用了 (=>) 符号, 这是 Dart 中单行函数或方法的简写。
 void main() => runApp(new MyApp());
 
-// 该应用程序继承了 StatelessWidget，这将会使应用本身也成为一个 widget。
-// 在 Flutter 中，大多数东西都是 widget，包括对齐 (alignment)、填充 (padding) 和布局 (layout)
-
-// 本示例中的 body 的 widget 树中包含了一个 Center widget, Center widget 又包含一个 Text 子 widget。
-// Center widget 可以将其子 widget 树对其到屏幕中心。
 class MyApp extends StatelessWidget {
-  // widget 的主要工作是提供一个 build() 方法来描述如何根据其他较低级别的 widget 来显示自己。
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Startup Name Generator',
-      theme: new ThemeData(
-          //主题颜色
-          //primaryColor: Colors.white,
-          ),
-      home: new RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  /*@override
-  State<RandomWords> createState() {
-    return new RandomWordsState();
-  }*/
-  @override
-  createState() => new RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  // 该变量以下划线（_）开头，在 Dart 语言中使用下划线前缀标识符，会强制其变成私有的。
-  final _suggestions = <WordPair>[];
-
-  // 集合存储用户喜欢（收藏）的单词对
-  final _saved = new Set<WordPair>();
-
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-        builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile
-              .divideTiles(
-                context: context,
-                tiles: tiles,
-              )
-              .toList();
-
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text('Saved Suggestions'),
+      title: 'Flutter Demo',
+      theme: new ThemeData.light(),
+      home: Scaffold(
+        body: Center(
+          child: Text('跟着JSPang(胖)学习Flutter，一起加油吧！',
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+            style: TextStyle(
+                fontSize: 23.0,
+                fontStyle: FontStyle.italic,
+                color: Color.fromARGB(255,255, 125, 125),
+                decoration: TextDecoration.underline,
+                decorationStyle: TextDecorationStyle.solid
             ),
-            body: new ListView(children: divided),
-          );
-        },
+          ),
+        ),
       ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-//    final wordPair = new WordPair.random();
-//    return new Text(wordPair.asPascalCase);
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
-        // 在偶数行，该函数会为单词对添加一个ListTile row.
-        // 在奇数行，该行书湖添加一个分割线widget，来分隔相邻的词对。
-        // 注意，在小屏幕上，分割线看起来可能比较吃力。
-        itemBuilder: (context, i) {
-          // 在每一列之前，添加一个1像素高的分隔线widget
-          if (i.isOdd) return new Divider();
-
-          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
-          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
-          final index = i ~/ 2;
-          // 如果是建议列表中最后一个单词对
-          if (index >= _suggestions.length) {
-            // ...接着再生成10个单词对，然后添加到建议列表
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
     );
   }
 }
