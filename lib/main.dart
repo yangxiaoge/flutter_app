9e13e5e0acc71b4f7ff67e0b3a65bcc02dd19ea7
yangjianan <yangjianan@seuic.com>
2018-12-14 16:02:31 +0800

    Positioned层叠定位组件

--- lib/main.dart
+++ lib/main.dart
@@ -21,21 +21,33 @@ class StackWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     // 定义层叠布局
     var stack = Stack(
-      alignment:FractionalOffset(0.5, 1), //AlignmentDirectional.bottomCenter, //让Container布局居下(可以用多种方式实现)
+      alignment: FractionalOffset(0.5,
+          1), //AlignmentDirectional.bottomCenter, //让Container布局居下(可以用多种方式实现)
       children: <Widget>[
         CircleAvatar(
           backgroundImage: NetworkImage(
               'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2541280047.jpg'),
           radius: 100.0, //圆形弧度
         ),
-        Container(
-          decoration: BoxDecoration(color: Colors.redAccent),
-          padding: EdgeInsets.all(5.0),
-          child: Text('小猪猪你好',
-          style: TextStyle(color: Colors.white),),
-        )
+        Positioned(
+          top: 10.0,
+          left: 35.0,
+          child: Text(
+            'yangxiaoge.github.io',
+            style: TextStyle(color: Color(0xffff0000)),
+          ),
+        ),
+        Positioned(
+          bottom: 10.0,
+          right: 60.0,
+          child: Text(
+            'yangxiaoge',
+          style: TextStyle(color: Colors.redAccent)),
+        ),
       ],
     );
-    return Center(child: stack,);
+    return Center(
+      child: stack,
+    );
   }
 }
