ff25daac2376d91d041a0d328c4f2dae795a12ac
yangjianan <yangjianan@seuic.com>
2018-12-19 17:21:29 +0800

    静态图片资源加载

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: "页面跳转返回数据",
-    home: FirstPage(),
-  ));
-}
+void main() => runApp(MyApp());
 
-class FirstPage extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('页面1'),
-      ),
-      body: Center(
-        child: RouteButton(),
-      ),
-    );
-  }
-}
-
-class RouteButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton(
-      onPressed: () {
-        _navigateToSecond(context);
-      },
-      child: Text('去第二页'),
-    );
-  }
-
-  _navigateToSecond(BuildContext context) async { //异步请求
-    final result = await Navigator.push( //等待资源回来之后显示
-        context, MaterialPageRoute(builder: (context) => SecondPage()));
-    Scaffold.of(context).showSnackBar(SnackBar(content: Text(result)));
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('页面2'),
-      ),
-      body: Center(
+    return MaterialApp(
+      title: 'Image',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Image'),
+        ),
+        body: Container(
           child: Column(
-        children: <Widget>[
-          RaisedButton(
-            child: Text('携带数据返回上一页-1'),
-            onPressed: () {
-              Navigator.pop(context, '返回数据111');
-            },
+            children: <Widget>[
+              Text(
+                '本地图片：',
+                style: TextStyle(color: Colors.red),
+              ),
+              Image.asset(
+                'images/funny1.jpeg',
+                width: 200,
+                height: 200,
+              ),
+              Divider(),
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      '网络图片：',
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                    Image.network(
+                      'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2541280047.webp',
+                      width: 140,
+                      height: 200,
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
-          RaisedButton(
-            child: Text('携带数据返回上一页-2'),
-            onPressed: () {
-              Navigator.pop(context, '返回数据222');
-            },
-          ),
-        ],
-      )),
+        ),
+      ),
     );
   }
 }
