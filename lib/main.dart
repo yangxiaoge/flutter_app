6af47f522fb8b9d33094181fcca297f3301404ea
yangjianan <yangjianan@seuic.com>
2018-12-19 13:43:56 +0800

    导航的参数传递和接受

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,65 @@
 import 'package:flutter/material.dart';
-import 'SecondPage.dart';
 
-void main() => runApp(MyApp());
+class Product {
+  final String title;
+  final String desc;
+  Product(this.title, this.desc);
+}
 
-class MyApp extends StatelessWidget {
+void main() {
+  runApp(MaterialApp(
+    title: '导航的数据传递和接收',
+    home: ProductListView(
+        products: List.generate(100, (i) => Product('商品 $i', '商品详情,编号为：$i'))),
+  ));
+}
+
+class ProductListView extends StatelessWidget {
+  final List<Product> products;
+
+  ProductListView({Key key, @required this.products}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '导航演示',
-      home: FirstPage(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('商品列表'),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, i) => Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text('${products[i].title}'),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              ProductDetail(itemProduct: products[i]),
+                        ));
+                  },
+                ),
+                Divider(color: Color(0xfff8bbd0),)
+              ],
+            ),
+      ),
     );
   }
 }
 
-class FirstPage extends StatelessWidget {
+class ProductDetail extends StatelessWidget {
+  final Product itemProduct;
+  ProductDetail({Key key, this.itemProduct}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('导航页面'),
-        ),
-        body: Center(
-          child: RaisedButton(
-            child: Text('跳转到SecondPage'),
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SecondPage(),
-                  ));
-            },
-          ),
-        ));
+      appBar: AppBar(
+        title: Text('商品详情页'),
+      ),
+      body: Center(
+        child: Text(itemProduct.desc),
+      ),
+    );
   }
 }
-
-// class SecondPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: Text('导航页面2'),
-//         ),
-//         body: Center(
-//           child: RaisedButton(
-//             child: Text('返回'),
-//             onPressed: () {
-//               Navigator.pop(context);
-//             },
-//           ),
-//         ));
-//   }
-// }
